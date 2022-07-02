94f1985fd390d8d21aa502b2504175577dce8996
iKabacinski <igor.kabacinski@gmail.com>
2022-07-02 10:53:05 +0200

    then and wrap expample

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -2,11 +2,17 @@
 
 describe('Our first suite', () => {
 
-    it('first test', () => {
+    beforeEach(() => {
+        // root-level hook
+        // runs before every test block
 
         cy.visit('/');
         cy.contains('Forms').click();
         cy.contains('Form Layout').click();
+      })
+
+    it('first test', () => {
+
         
         //get by tag name
         cy.get('input');
@@ -42,12 +48,8 @@ describe('Our first suite', () => {
 
     })
 
-    it.only('Second test', () => {
+    it('Second test', () => {
         
-        cy.visit('/');
-        cy.contains('Forms').click();
-        cy.contains('Form Layout').click();
-      
         cy.get('[data-cy="signInButton"]').click();
 
         cy.contains('Sign in').click();
@@ -65,6 +67,38 @@ describe('Our first suite', () => {
         cy.contains('nb-card','Horizontal form').find('[type="email"]');
     })
 
+    it.only('then and wrap methods', () => {
 
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain','Email');
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain','Password');
+
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain','Email address');
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain','Password');
+
+        // Selenium style, not gonna work because cypress works asynchronous :) 
+        // const firstForm = cy.contains('nb-card', 'Using the Grid');
+        // firstForm.find('[for="inputEmail1"]').should('contain','Email');
+        // firstForm.find('[for="inputPassword2"]').should('contain','Password');
+
+        //Cypress style
+        cy.contains('nb-card', 'Using the Grid').then( firstForm => {
+            const emailLabelfirst = firstForm.find('[for="inputEmail1"]').text();
+            const passwordLabelfirst = firstForm.find('[for="inputPassword2"]').text();
+            
+            expect(emailLabelfirst).to.equal('Email');
+            expect(passwordLabelfirst).to.equal('Password');
+
+            cy.contains('nb-card', 'Basic form').then (secondForm => {
+                
+                const passwordSecondLabel = secondForm.find('[for="exampleInputPassword1"]').text();
+                expect(passwordSecondLabel).to.equal(passwordLabelfirst);
+                
+                //back to cyppress context, wrap value from function parameter
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain','Password');
+
+            })
+        })
+
+    })
 
 })
