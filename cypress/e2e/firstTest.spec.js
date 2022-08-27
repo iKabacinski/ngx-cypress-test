406c25c42917ccd0a8d1239fa15537916e78fa4e
iKabacinski <igor.kabacinski@gmail.com>
2022-08-27 16:55:41 +0200

    Web tables handling

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -178,7 +178,7 @@ describe('Our first suite', () => {
 
     })
 
-    it.only('lists and dropdowns', ()=> {
+    it('lists and dropdowns', ()=> {
         //1
         // cy.get('nav nb-select').click();
         // cy.get('.options-list').contains("Dark").click();
@@ -208,6 +208,64 @@ describe('Our first suite', () => {
  
             });
         });
+
+    })
+
+    it.only('Web tables', ()=>{
+        cy.contains('Tables & Data').click();
+        cy.contains('Smart Table').click();
+
+        //1 editing alredy existing value
+        cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click();
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25');
+            cy.wrap(tableRow).find('.nb-checkmark').click();
+
+            cy.wrap(tableRow).find('td').eq(6).should('have.text','25');
+        })
+
+        //2 adding new value
+        cy.get('thead').find('.nb-plus').click()
+
+        cy.get('thead').find('tr').eq(2).then(tableRow =>{
+
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type('Igor');
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Kabacinski');
+            cy.wrap(tableRow).find('[placeholder="Username"]').type('isiu');
+            cy.wrap(tableRow).find('[placeholder="E-mail"]').type('igor.kabacinski@gmail.com');
+            cy.wrap(tableRow).find('[placeholder="Age"]').type('26');
+
+            cy.wrap(tableRow).find('.nb-checkmark').click();
+        });
+
+        cy.get('tbody tr').first().find('td').then(tableColumns =>{
+            cy.wrap(tableColumns).eq(2).should('contain','Igor');
+            cy.wrap(tableColumns).eq(3).should('contain','Kabacinski');
+        });
+
+        //3 searching 
+
+        const age = [20,30,40,200];
+
+        cy.wrap(age).each(age => {
+
+            cy.get('thead [placeholder="Age"]').clear().type(age);
+            cy.wait(500);
+            cy.get('tbody tr').each(tableRow => {
+                if(age == 200) {
+                    cy.wrap(tableRow).should('contain','No data found');
+                } 
+                else {
+                    cy.wrap(tableRow).find('td').eq(6).should('have.text',age);
+                }
+            });
+        });
+
+        // cy.get('thead [placeholder="Age"]').type('20');
+        // cy.wait(500);
+        // cy.get('tbody tr').each(tablerow => {
+        //     cy.wrap(tablerow).find('td').eq(6).should('have.text','20');
+        // });
 
     })
 
