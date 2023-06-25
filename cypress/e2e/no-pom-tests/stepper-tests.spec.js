9b3b5e81a832b8be8b3dba1c91ab37d4e4e7584b
iKabacinski <igor.kabacinski@gmail.com>
2023-06-25 19:56:41 +0200

    second stepper tests with fixture file and dual commands

--- cypress/e2e/no-pom-tests/stepper-tests.spec.js
+++ cypress/e2e/no-pom-tests/stepper-tests.spec.js
@@ -1,29 +1,64 @@
 /// <reference types="cypress"/>
 
 describe('Stepper form test', () => {
-
-    beforeEach(() => {
-        cy.visit('/');
-        cy.selectFormFromMenu('Layout','Stepper');
-    });
     
+    let contents;
+
+    before(() => {
+        cy.fixture('steppersContent').then((steppersContent) => {
+            contents = steppersContent;
+        });
+    });
+
+    beforeEach(() => {    
+        cy.visit('/');
+        cy.selectFormFromMenu('Layout', 'Stepper');
+    });
+
     it('Should all steppers be on the page', () => {
-        cy.url().should('include','pages/layout/stepper');
+        cy.url().should('include', 'pages/layout/stepper');
         cy.get('ngx-stepper')
             .find('nb-stepper').should('have.length', 3);
     });
 
-    it.only('Should finish first process', () => {
-        cy.get('nb-stepper[orientation="horizontal"]').within(()=>{
+    it('Should finish first stepper', () => {
+        cy.get('nb-stepper[orientation="horizontal"]').within(() => {
 
-            cy.get('.header').find('div.step').first().should('have.class','selected');
-
-            cy.contains('button','prev').should('be.disabled');
-            cy.contains('button','next').should('be.enabled');
+            cy.get('.header').find('div.step').first().should('have.class', 'selected');
 
             cy.get('.step-content')
-                .find('h3').should('have.text','Step content #1');
-        });
+                .find('h3').should('have.text', 'Step content #1');
 
+            cy.contains('button', 'prev').should('be.disabled');
+            cy.navigateToNextStepAndVerifyContent('Step content #2');
+
+            cy.contains('button', 'prev').should('be.enabled');
+            cy.navigateToNextStepAndVerifyContent('Step content #3');
+
+            cy.contains('button', 'prev').should('be.enabled');
+            cy.navigateToNextStepAndVerifyContent('Step content #4');
+
+        });
+    });
+
+    it('Should finish second stepper', () => {
+        cy.get('nb-stepper[orientation="vertical"]').within(() => {
+
+            cy.get('.header').find('div.step').first().should('have.class', 'selected');
+
+            cy.get('.step-content')
+                .find('h3').should('have.text', 'Step content #1');
+
+            cy.contains('button', 'prev').should('be.disabled');
+            cy.navigateToNextStepAndVerifyContent('Step content #2',contents.secondStepper.stepperContent[1]);
+           
+            cy.contains('button', 'prev').should('be.enabled');
+            cy.navigateToNextStepAndVerifyContent('Step content #3',contents.secondStepper.stepperContent[2]);
+            //TODO add second paragraph verification
+
+            cy.contains('button', 'prev').should('be.enabled');
+            cy.navigateToNextStepAndVerifyContent('Step content #4',contents.secondStepper.stepperContent[3]);
+
+        });
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,17 @@
 /// <reference types="cypress" />
 
-Cypress.Commands.add('selectFormFromMenu',  (firstOption, secondaryOption)=>{
+Cypress.Commands.add('selectFormFromMenu', (firstOption, secondaryOption) => {
     cy.contains(firstOption).should('exist').click();
     cy.contains(secondaryOption).should('be.visible').click();
+});
+
+Cypress.Commands.add('navigateToNextStepAndVerifyContent', (title, content) => {
+    cy.contains('button', 'next').should('be.enabled').click().then(() => {
+        cy.get('.step-content')
+            .find('h3').should('have.text', title);
+        if (content) {
+            cy.get('.step-content')
+                .find('p').should('contain.text', (content));
+        }
+    });
 });
