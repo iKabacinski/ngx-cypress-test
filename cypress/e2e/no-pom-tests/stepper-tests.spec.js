8cf4c4357e680b05ab0a001045b12363d3c9b4d0
iKabacinski <igor.kabacinski@gmail.com>
2023-07-02 19:33:54 +0200

    last stepper test and refactor

--- cypress/e2e/no-pom-tests/stepper-tests.spec.js
+++ cypress/e2e/no-pom-tests/stepper-tests.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 
 describe('Stepper form test', () => {
-    
+
     let contents;
 
     before(() => {
@@ -10,7 +10,7 @@ describe('Stepper form test', () => {
         });
     });
 
-    beforeEach(() => {    
+    beforeEach(() => {
         cy.visit('/');
         cy.selectFormFromMenu('Layout', 'Stepper');
     });
@@ -24,39 +24,80 @@ describe('Stepper form test', () => {
     it('Should finish first stepper', () => {
         cy.get('nb-stepper[orientation="horizontal"]').within(() => {
 
-            cy.get('.header').find('div.step').first().should('have.class', 'selected');
-
-            cy.get('.step-content')
-                .find('h3').should('have.text', 'Step content #1');
+            cy.verifyTitle('Step content #1');
 
             cy.contains('button', 'prev').should('be.disabled');
-            cy.navigateToNextStepAndVerifyContent('Step content #2');
+            cy.navigateToNextStep().then(() => {
+                cy.verifyTitle('Step content #2');
+            });
+            cy.contains('button', 'prev').should('be.enabled');
+            cy.navigateToNextStep().then(() => {
+                cy.verifyTitle('Step content #3');
+            });
 
             cy.contains('button', 'prev').should('be.enabled');
-            cy.navigateToNextStepAndVerifyContent('Step content #3');
-
-            cy.contains('button', 'prev').should('be.enabled');
-            cy.navigateToNextStepAndVerifyContent('Step content #4');
+            cy.navigateToNextStep().then(() => {
+                cy.verifyTitle('Step content #4');
+                cy.contains('button', 'next').should('be.disabled');
+            });
 
         });
     });
 
-    it.only('Should finish second stepper', () => {
+    it('Should finish second stepper', () => {
+        cy.get('.steppers-container').find('nb-stepper.horizontal').within(() => {
+
+            cy.contains('button', 'prev').should('not.exist');
+            cy.verifyContent(contents.secondStepper.content[0]);
+            cy.get('.form-control').type(contents.secondStepper.inputValue[0]);
+
+            cy.navigateToNextStep().then(() => {
+                cy.verifyContent(contents.secondStepper.content[1]);
+                cy.get('.form-control').type(contents.secondStepper.inputValue[1]);
+            });
+
+            cy.contains('button', 'prev').should('be.enabled');
+            cy.navigateToNextStep().then(() => {
+                cy.verifyContent(contents.secondStepper.content[2]);
+                cy.get('.form-control').type(contents.secondStepper.inputValue[2]);
+            });
+
+            cy.contains('button', 'prev').should('be.enabled');
+            cy.contains('button', 'Confirm').should('be.exist').and('be.enabled').click().then(() => {
+                cy.verifyTitle(contents.secondStepper.successMessege)
+            });
+
+            cy.contains('button', 'Try again').should('exist').click().then(() => {
+                cy.contains('button', 'prev').should('not.exist');
+                cy.verifyContent(contents.secondStepper.content[0]);
+            });
+        });
+    });
+
+
+    it('Should finish third stepper', () => {
         cy.get('nb-stepper[orientation="vertical"]').within(() => {
 
-            cy.get('.header').find('div.step').first().should('have.class', 'selected');
-
-            cy.get('.step-content')
-                .find('h3').should('have.text', 'Step content #1');
-
             cy.contains('button', 'prev').should('be.disabled');
-            cy.navigateToNextStepAndVerifyContent('Step content #2',contents.secondStepper.firstParagraph[1]);
-           
-            cy.contains('button', 'prev').should('be.enabled');
-            cy.navigateToNextStepAndVerifyContent('Step content #3',contents.secondStepper.firstParagraph[2], contents.secondStepper.secondParagraph[0]);
+            cy.verifyTitle('Step content #1');
+            cy.verifyContent(contents.thirdStepper.firstParagraph[0]);
+
+            cy.navigateToNextStep().then(() => {
+                cy.verifyTitle('Step content #2');
+                cy.verifyContent(contents.thirdStepper.firstParagraph[1]);
+            });
 
             cy.contains('button', 'prev').should('be.enabled');
-            cy.navigateToNextStepAndVerifyContent('Step content #4',contents.secondStepper.firstParagraph[3]);
+            cy.navigateToNextStep().then(() => {
+                cy.verifyTitle('Step content #3');
+                cy.verifyContent(contents.thirdStepper.firstParagraph[2], contents.thirdStepper.secondParagraph[0]);
+            });
+
+            cy.contains('button', 'prev').should('be.enabled');
+            cy.navigateToNextStep().then(() => {
+                cy.verifyTitle('Step content #4');
+                cy.verifyContent(contents.thirdStepper.firstParagraph[3]);
+            });
             cy.contains('button', 'next').should('be.disabled');
 
         });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,17 +5,20 @@ Cypress.Commands.add('selectFormFromMenu', (firstOption, secondaryOption) => {
     cy.contains(secondaryOption).should('be.visible').click();
 });
 
-Cypress.Commands.add('navigateToNextStepAndVerifyContent', (title, firstParagraph, secondParagraph) => {
-    cy.contains('button', 'next').should('be.enabled').click().then(() => {
+Cypress.Commands.add('navigateToNextStep', () => {
+    cy.contains('button', 'next').should('be.enabled').click()
+});
+
+Cypress.Commands.add('verifyTitle', (title) => {
+    cy.get('.step-content')
+        .find('h3').should('have.text', title);
+});
+
+Cypress.Commands.add('verifyContent', (firstParagraph, secondParagraph) => {
+    cy.get('.step-content')
+        .find('p').first().should('contain.text', (firstParagraph));
+    if (secondParagraph) {
         cy.get('.step-content')
-            .find('h3').should('have.text', title);
-        if (firstParagraph) {
-            cy.get('.step-content')
-                .find('p').first().should('contain.text', (firstParagraph));
-        }
-        if (secondParagraph) {
-            cy.get('.step-content')
-                .find('p').eq(1).should('contain.text', (secondParagraph));
-        }
-    });
+            .find('p').eq(1).should('contain.text', (secondParagraph));
+    }
 });
