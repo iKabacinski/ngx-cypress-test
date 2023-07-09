a78f99c7f3b674ffb2a83bb8f812577dce9afb4a
iKabacinski <igor.kabacinski@gmail.com>
2023-07-09 19:33:42 +0200

    hide tests fro  udemy course

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    excludeSpecPattern: ['**/1-getting-started/*','**/2-advanced-examples/*'],
+    excludeSpecPattern: ['**/1-getting-started/*','**/2-advanced-examples/*','**/udemy-course'],
   }
 })
