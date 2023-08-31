f0eac52ab83c3b9baa4064ef79f363b7b4d25d9c
iKabacinski <igor.kabacinski@gmail.com>
2023-08-31 18:30:57 +0200

    fix tests

--- cypress/support/acordion-commands.js
+++ cypress/support/acordion-commands.js
@@ -1,13 +1,13 @@
 Cypress.Commands.add('clickItemAndVerifyIfOtherAreCollapsed', (item) => {
     cy.getByDataCy(item).findByDataCy('header').click().then(() => {
-        cy.get('nb-accordion-item').not(`[data-cy="${item}"]`).should('have.class', 'expanded');
+        cy.get('nb-accordion-item').not(`[data-cy="${item}"]`).should('have.class', 'collapsed');
     });
 });
 
 Cypress.Commands.add('openAllItemInOneAccordion', (accordion) => {
     cy.getByDataCy(accordion).within(() => {
         cy.get('nb-accordion-item-header').click({ multiple: true }).then(() => {
-            cy.get('nb-accordion-item').should('have.class', 'collapsed');
+            cy.get('nb-accordion-item').should('have.class', 'expanded');
         });
     });
 });
@@ -15,7 +15,7 @@ Cypress.Commands.add('openAllItemInOneAccordion', (accordion) => {
 Cypress.Commands.add('closeAllItemInOneAccordion', (accordion) => {
     cy.getByDataCy(accordion).within(() => {
         cy.get('nb-accordion-item-header').click({ multiple: true }).then(() => {
-            cy.get('nb-accordion-item').should('have.class', 'expanded');
+            cy.get('nb-accordion-item').should('have.class', 'collapsed');
         });
     });
 });
